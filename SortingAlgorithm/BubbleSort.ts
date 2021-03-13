664417fc2bcb11ed98e3ac4b9aafe15c29bb7c3f
asif <mhamudasif2@gmail.com>
2021-03-13 07:55:22 +0600

    bubbleSort added

---
+++ SortingAlgorithm/BubbleSort.ts
@@ -0,0 +1,21 @@
+let arra = [1, 12, 3, 40, 9];
+
+function bubbleSort(arr: any) {
+  console.log(arra, "ara");
+
+  let i: number, j: number;
+  let endIndex = arr.length - 1;
+
+  for (i = 0; i <= endIndex - 1; i++) {
+    for (j = 0; j <= endIndex - (1 + i); j++) {
+      let temp = Math.max(arra[j], arra[j + 1]);
+      arra[j] = Math.min(arra[j], arra[j + 1]);
+      arra[j + 1] = temp;
+      console.log(arra[j], arra[j + 1]);
+    }
+  }
+}
+
+bubbleSort(arra);
+
+console.log(arra);
