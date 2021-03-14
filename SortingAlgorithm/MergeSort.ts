50f2734fd587589d501a83223ce6b686471262a2
asif <mhamudasif2@gmail.com>
2021-03-14 08:19:24 +0600

    mergeSort added

---
+++ SortingAlgorithm/MergeSort.ts
@@ -0,0 +1,52 @@
+//let array : number[] = [5,5,1,5,9,6,10] as number[]
+let array: number[] = [1, 5, 10, 6, 5, 5, 6] as number[];
+
+function merge(head: number, mid: number, end: number) {
+  let left: number[] = [],
+    right: number[] = [];
+
+  for (let i = head; i <= mid; i++) {
+    left.push(array[i]);
+  }
+  for (let j = mid + 1; j <= end; j++) {
+    right.push(array[j]);
+  }
+
+  let i = 0,
+    j = 0,
+    temp,
+    n = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      temp = left[i++];
+    } else {
+      temp = right[j++];
+    }
+    array[head++] = temp;
+  }
+
+  while (i < left.length) {
+    array[head++] = left[i++];
+  }
+  while (j < right.length) {
+    array[head++] = right[j++];
+  }
+  console.log(array, left, right);
+}
+
+function mergeSort(head: number, end: number) {
+  if (head >= end) return;
+
+  let mid = Math.floor((head + end) / 2);
+
+  mergeSort(head, mid);
+  mergeSort(mid + 1, end);
+
+  merge(head, mid, end);
+}
+
+console.log(array.length, "len");
+mergeSort(0, array.length - 1);
+
+console.log(array);
